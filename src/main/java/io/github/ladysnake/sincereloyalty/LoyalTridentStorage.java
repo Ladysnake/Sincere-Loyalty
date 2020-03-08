aa9ef40cb44c5dc7a1dec887cb345e514506f2f3
Pyrofab <redstoneinfire@gmail.com>
2020-03-08 17:54:08 +0100

    Add license headers

--- src/main/java/io/github/ladysnake/sincereloyalty/LoyalTridentStorage.java
+++ src/main/java/io/github/ladysnake/sincereloyalty/LoyalTridentStorage.java
@@ -1,3 +1,20 @@
+/*
+ * Sincere Loyalty
+ * Copyright (C) 2020 Ladysnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
 package io.github.ladysnake.sincereloyalty;
 
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
@@ -14,7 +31,9 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.PersistentState;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public final class LoyalTridentStorage extends PersistentState {
 
